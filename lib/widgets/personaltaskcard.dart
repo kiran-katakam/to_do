59e3b8c18b34c06889d38db6d221647fa7ba606f
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2024-12-26 12:46:10 +0530

    created file to display task card

---
+++ lib/widgets/personaltaskcard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PersonalTaskCard extends StatelessWidget {
+  const PersonalTaskCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

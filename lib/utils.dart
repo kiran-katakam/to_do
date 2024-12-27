775c6c8a0304a16b5772014a8686e421646df9ac
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2024-12-27 15:10:46 +0530

    Added constamts

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,6 @@
 String toDDMMYYYY(DateTime date) {
   return "${date.day}-${date.month}-${date.year}";
 }
+
+const String personalTaskCountKey = "personalTaskCount";
+const String personalTaskKeyPrefix = "personalTask";

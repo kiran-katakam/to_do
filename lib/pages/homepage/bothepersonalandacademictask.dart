cd9fd40ce2862103f6f8af02ab4d34326b4c0e6c
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2024-12-30 16:13:15 +0530

    Widget for both Personal and Academic Tasks

---
+++ lib/pages/homepage/bothepersonalandacademictask.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/pages/homepage/onlyacademictasks.dart';
+import 'package:to_do/pages/homepage/onlypersonaltasks.dart';
+
+class BothPersonalAndAcademicTasks extends StatefulWidget {
+  const BothPersonalAndAcademicTasks(
+      {super.key, required this.academicTasks, required this.personalTasks});
+
+  final List<String> academicTasks;
+  final List<String> personalTasks;
+
+  @override
+  State<BothPersonalAndAcademicTasks> createState() =>
+      _BothPersonalAndAcademicTasksState();
+}
+
+class _BothPersonalAndAcademicTasksState
+    extends State<BothPersonalAndAcademicTasks> {
+  bool academicIsExpanded = true;
+  bool personalIsExpanded = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 16),
+        child: ExpansionPanelList(
+          materialGapSize: 0,
+          expansionCallback: (panelIndex, isExpanded) {
+            setState(() {
+              panelIndex == 0
+                  ? academicIsExpanded = !academicIsExpanded
+                  : personalIsExpanded = !personalIsExpanded;
+            });
+          },
+          expandIconColor: const Color.fromRGBO(205, 185, 252, 1),
+          expandedHeaderPadding: const EdgeInsets.all(0),
+          animationDuration: const Duration(milliseconds: 500),
+          children: [
+            ExpansionPanel(
+              canTapOnHeader: true,
+              isExpanded: academicIsExpanded,
+              headerBuilder: (context, isExpanded) {
+                return const Text(
+                  "Academic Tasks",
+                  style: TextStyle(fontSize: 24),
+                );
+              },
+              body: OnlyAcademicTasks(academicTasks: widget.academicTasks),
+            ),
+            ExpansionPanel(
+              canTapOnHeader: true,
+              isExpanded: personalIsExpanded,
+              headerBuilder: (context, isExpanded) {
+                return const Text(
+                  "Personal Tasks",
+                  style: TextStyle(fontSize: 24),
+                );
+              },
+              body: OnlyPersonalTasks(personalTasks: widget.personalTasks),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
